a8007da668b5b9db7e723dea3b8071bd5d0fc6b8
rurulmbrttgd <ronaldlambertnicolastugadi@gmail.com>
2024-01-26 02:08:04 +0800

    okay

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -12,26 +12,6 @@ export interface LoginProps {
 export const Login = ({ className }: LoginProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <div className={styles['logo-div']}>
-                <img
-                    src="https://res.cloudinary.com/dxzvh2xex/image/upload/v1706204573/salesoptima-logo_kpbyj3.png"
-                    alt=""
-                    className={styles['login-logo']}
-                />
-            </div>
-            <svg
-                width="148"
-                height="1080"
-                viewBox="0 0 148 1080"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M103.5 1080L144.974 866.644C149.509 843.314 145.587 819.131 133.909 798.43L13.928 585.736C-2.84687 555.999 -3.29059 519.758 12.7512 489.619L134.883 260.159C145.939 239.387 149.316 215.383 144.42 192.367L103.5 0"
-                    stroke="white"
-                    className={styles['middle-line']}
-                />
-            </svg>
             <div className={styles['login-div']}>
                 <div>
                     <h4>Heading 4</h4>
@@ -47,6 +27,26 @@ export const Login = ({ className }: LoginProps) => {
                     <h4>Heading 4</h4>
                 </div>
                 <h4>Heading 4</h4>
+            </div>
+            <svg
+                width="148"
+                height="1080"
+                viewBox="0 0 148 1080"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M103.5 1080L144.974 866.644C149.509 843.314 145.587 819.131 133.909 798.43L13.928 585.736C-2.84687 555.999 -3.29059 519.758 12.7512 489.619L134.883 260.159C145.939 239.387 149.316 215.383 144.42 192.367L103.5 0"
+                    stroke="white"
+                    className={styles['middle-line']}
+                />
+            </svg>
+            <div className={styles['logo-div']}>
+                <img
+                    src="https://res.cloudinary.com/dxzvh2xex/image/upload/v1706204573/salesoptima-logo_kpbyj3.png"
+                    alt=""
+                    className={styles['login-logo']}
+                />
             </div>
         </div>
     );
