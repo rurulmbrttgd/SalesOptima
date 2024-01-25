1bba9006beeb7e672c5a60e424a420fa30e2cd75
riangracej <hannahgraceajoaquin@gmail.com>
2024-01-26 03:30:15 +0800

    app

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,49 @@ function App() {
 
     return (
         <div className={styles.App}>
-            <Login></Login>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <ViteLogo
+                        height="6em"
+                        width="6em"
+                        className={classNames(styles.logo)}
+                        title="Vite logo"
+                    />
+                </a>
+                <a href="https://reactjs.org" target="_blank">
+                    <ReactLogo
+                        height="6em"
+                        width="6em"
+                        className={classNames(styles.logo, styles.react)}
+                        title="React logo"
+                    />
+                </a>
+                <a href="https://www.typescriptlang.org/" target="_blank">
+                    <TypescriptLogo
+                        height="6em"
+                        width="6em"
+                        className={classNames(styles.logo, styles.ts)}
+                        title="Typescript logo"
+                    />
+                </a>
+                <a href="https://sass-lang.com/" target="_blank">
+                    <ScssLogo
+                        height="6em"
+                        width="6em"
+                        className={classNames(styles.logo, styles.scss)}
+                        title="SCSS logo"
+                    />
+                </a>
+            </div>
+            <div className={styles.card}>
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className={styles['read-the-docs']}>
+                Click on the Vite and React logos to learn more
+            </p>
         </div>
     );
 }
