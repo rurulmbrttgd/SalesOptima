05c761f930159e6ef948e3862c4c0c0ed5bfe04b
rurulmbrttgd <ronaldlambertnicolastugadi@gmail.com>
2024-01-26 17:55:54 +0800

    5:55pm

---
+++ src/_codux/boards/dashboard.board.tsx
@@ -0,0 +1,26 @@
+import { createBoard } from '@wixc3/react-board';
+import { Topbar } from '../../components/topbar/topbar';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { Dashboard } from '../../components/dashboard/dashboard';
+import Dashboard_module from '../../components/dashboard/dashboard.module.scss';
+
+export default createBoard({
+    name: 'Home',
+    Board: () => (
+        <div className={Dashboard_module['home-dashboard']}>
+            <div>
+                <Topbar />
+            </div>
+            <div className={Dashboard_module.content}>
+                <Sidebar />
+                <Dashboard />
+            </div>
+        </div>
+    ),
+    isSnippet: true,
+    environmentProps: {
+        canvasWidth: 2000,
+        windowWidth: 1920,
+        windowHeight: 1080,
+    },
+});
--- src/_codux/boards/dashborad.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-
-export default createBoard({
-    name: 'Dashboard',
-    Board: () => <div></div>,
-    isSnippet: true,
-});
